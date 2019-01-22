7bee0cdd1c53b4d11f4164bf5084ccd53044eee7
Chris Mears <chris.mears@gmail.com>
2019-01-22 10:43:23 -0800

    Add contact link

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,6 +10,19 @@ const IndexPage = () => (
     <div style={{ width: `100%`, margin: `1.45rem 0` }}>
       <Image />
     </div>
+    <div
+      style={{
+        fontFamily: `sans-serif`,
+        textAlign: `center`
+      }}
+    >
+      <a
+        href="mailto:info@wanderingleafstudios.com"
+        style={{ color: `#588F27` }}
+      >
+        Contact
+      </a>
+    </div>
   </Layout>
 )
 

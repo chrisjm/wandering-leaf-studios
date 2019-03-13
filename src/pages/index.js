2a9811b90f1c14ecba20dac164a9b22288c31a8c
Chris Mears <chris.mears@gmail.com>
2019-03-12 20:03:03 -0700

    Use SVG logo

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,17 @@
 import React from 'react'
 
-import logo from '../images/wandering-leaf-studios-logo.png'
+import logo from '../images/wandering_leaf_studios_color_logo_transparent_background.svg'
 import SEO from '../components/seo'
 
 const IndexPage = () => (
   <div className="bg-green text-white">
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <div className="bg-white p-10">
+    <div className="bg-white p-5 lg:p-10">
       <div className="container mx-auto">
-        <img src={logo} alt="Wandering Leaf Studios Logo" />
+        <img src={logo} alt="Wandering Leaf Studios Logo" className="w-full m-0" />
       </div>
     </div>
-    <div className="container mx-auto p-10 text-center bg-green">
+    <div className="container mx-auto p-5 lg:p-10 text-center bg-green">
       <a href="mailto:info@wanderingleafstudios.com" className="text-white">
         Contact
       </a>
